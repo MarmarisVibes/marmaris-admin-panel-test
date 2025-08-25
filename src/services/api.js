4d2f1619a74deb5af61ef088134b7c7720d2d8b6
MarmarisVibes <marmarisvibes25@gmail.com>
2025-08-25 23:32:42 +0100

    Update API URL to live backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://marmaris-admin-panel.onrender.com/api';
 
 export const api = axios.create({ baseURL: API_BASE });
 
